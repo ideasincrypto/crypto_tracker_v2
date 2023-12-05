a3d5da579fd1b9cb5326b795ed4ba9bebf311c33
Frederico <frederico.smozzato@gmail.com>
2023-12-05 12:21:26 -0300

    Create portfolio table

--- spec/system/home/user_visits_home_page_spec.rb
+++ spec/system/home/user_visits_home_page_spec.rb
@@ -7,5 +7,27 @@ describe "User visits the home page" do
 
       expect(page).to have_content "CryptoTracker"
     end
+
+    it "and views the table" do
+      visit root_path
+
+      expect(page).to have_selector "table"
+      within("table") do
+        expect(page).to have_selector "thead"
+        within("thead") do
+          expect(page).to have_selector "th", text: "Coin"
+          expect(page).to have_selector "th", text: "Amount"
+          expect(page).to have_selector "th", text: "Rate (USD)"
+          expect(page).to have_selector "th", text: "24h change"
+          expect(page).to have_selector "th", text: "Value"
+          expect(page).to have_selector "th", text: "%"
+        end
+        expect(page).to have_selector "tbody"
+        expect(page).to have_selector "tfoot"
+        within("tfoot") do
+          expect(page).to have_selector "td", text: "TOTAL"
+        end
+      end
+    end
   end
 end
