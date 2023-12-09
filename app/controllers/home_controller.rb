63259d2037bae6f18d6bf9252ad9f2fd092026a3
Frederico <frederico.smozzato@gmail.com>
2023-12-09 16:16:14 -0300

    Creates a homepage for unauthenticated visitors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!, only: [:index]
 
   def index
-    @portfolios = current_user.account.portfolios.all
+    if user_signed_in?
+      @portfolios = current_user.account.portfolios.all
+    else
+      redirect_to visitors_path unless current_user
+    end
   end
+
+  def visitors; end
 end
