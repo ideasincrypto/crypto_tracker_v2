833b9b4d12170b693d1057c90dcbe25ccb204069
Frederico <frederico.smozzato@gmail.com>
2023-12-11 18:01:45 -0300

    Adds uniqueness validation to Coin model

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -2,6 +2,7 @@ class Coin < ApplicationRecord
   has_many :holdings
 
   validates :name, :api_id, :ticker, presence: true
+  validates :name, :api_id, :ticker, :icon, uniqueness: true
   validates :rate, numericality: { greater_than_or_equal_to: 0 }
 
   def set_rate(request_service)
