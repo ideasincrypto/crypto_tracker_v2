11a2642725983b9c625aaf9bf98311729193c796
Frederico <frederico.smozzato@gmail.com>
2023-12-14 20:57:59 -0300

    Implements portfolio total value calculation

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,13 +8,18 @@ class Portfolio < ApplicationRecord
   def refresh_rates(request_service)
     return if holdings.empty?
 
-    # debugger
-
     coins = self.coins
     rates = request_service.get_rates(coins)
     coins.each do |coin|
       rate = rates.dig(coin.api_id.to_sym, :usd)
       coin.update!(rate: rate)
+    end
+  end
+
+  def total_value
+    holdings.reduce(0.0) do |total, holding|
+      total += holding.amount * holding.coin.rate
+      total
     end
   end
 end
