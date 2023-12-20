044b4643d08eb91e8d581bef4673f08a0c4ca7ba
Frederico <frederico.smozzato@gmail.com>
2023-12-20 17:01:25 -0300

    Creates value and percentage methods for Asset

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,8 +1,17 @@
 class Asset
-  attr_accessor :coin, :amount
+  attr_accessor :coin, :amount, :account
 
-  def initialize(coin:, amount:)
+  def initialize(coin:, amount:, account:)
     @coin = coin
     @amount = amount
+    @account = account
+  end
+
+  def value
+    @coin.rate * @amount
+  end
+
+  def percentage
+    (value * 100 / @account.net_worth).round(2)
   end
 end
